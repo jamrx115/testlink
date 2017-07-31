cf793a888673a7a0b9a598d9ec1a169462b651ea
jamrx115 <jamrx115@gmail.com>
2017-07-31 13:41:54 -0700

    Primer commit testlink

---
+++ cfg/mantis.cfg.php
@@ -0,0 +1,17 @@
+ <?php 
+      define('BUG_TRACK_DB_HOST', 'localhost'); 
+ 
+      define('BUG_TRACK_DB_NAME', 'alltic_mant1'); 
+ 
+      define('BUG_TRACK_DB_USER', 'alltic_mant1'); 
+ 
+      define('BUG_TRACK_DB_PASS', 'G&xKIsEUav00#&1'); 
+ 
+      define('BUG_TRACK_HREF', "http://alltic.co/BT"); 
+ 
+      define('BUG_TRACK_DB_TYPE', 'mysql');
+
+      define('BUG_TRACK_HREF', "http://alltic.co/BT/view.php?id=");
+ 
+      define('BUG_TRACK_ENTER_BUG_HREF',"http://alltic.co/BT/");  
+      ?>
